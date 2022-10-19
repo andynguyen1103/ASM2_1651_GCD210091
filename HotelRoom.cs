c520bfbccec76a68add20dac85714bc470309905
Minh Duc <andynguyenvn1103@gmail.com>
2022-10-19 23:24:49 +0700

    Start the project

---
+++ HotelRoom.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asignment2
+{
+    internal class HotelRoom : IHotelRoom
+    {
+        private int _roomNo;
+        private bool _isAvailable;
+        private double _price;
+        private string _guest;
+        public int RoomNo 
+        {
+            get  => _roomNo; 
+            
+            set
+            {
+                if (value > 0)
+                {
+                    _roomNo = value;
+                }
+                else Console.WriteLine("Invalid input");
+            }
+        }
+        public bool IsAvailable 
+        { 
+            get => _isAvailable; 
+
+            set => _isAvailable=value;
+        }
+        public double Price 
+        { get => throw new NotImplementedException(); 
+          set => throw new NotImplementedException(); 
+        }
+        public string Guest { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string GetDescription()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IHotelRoom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asignment2
+{
+    internal interface IHotelRoom
+    {
+        int RoomNo { get; set; }
+        bool IsAvailable { get; set; }
+        double Price { get; set; }
+        string Guest { get; set; }
+
+        string GetDescription();
+    }
+}
